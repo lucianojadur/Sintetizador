3b5d758a66a82e68231f0f7edf728e84ff2b18e7
Luciano Jadur <66574334+lucianojadur@users.noreply.github.com>
2020-07-29 20:57:45 -0300

    Update nota.c

--- nota.c
+++ nota.c
@@ -4,7 +4,7 @@
 
 
 
-static char * _leer_linea(FILE *f);
+static char * _leer_linea(FILE *f, size_t size);
 
 static bool _obtener_datos_armonico(char * armonico_linea, const char * delim, float * f, float * a);
 
@@ -33,9 +33,9 @@ bool obtener_armonicos(FILE *f, nota_t *nota){
 	const char delim[2] = LINE_DELIMITER;
 	fgetc(f);
 	for (size_t i =  0; i < nota->cantidad_armonicos; i++){	//[cantidad de armonicos] líneas siguientes
-		if ((strcmp(linea = _leer_linea(f), "\0")) == 0) //malloc
+		if ((linea = _leer_linea(f, HARM_STR_LENGTH + 1)) == NULL) //malloc
 			return false;
-		if(!_obtener_datos_armonico(linea, delim, &nota->armonicos[i][REL_FREC_INDEX], &nota->armonicos[i][REL_AMP_INDEX]))
+		if (!_obtener_datos_armonico(linea, delim, &nota->armonicos[i][REL_FREC_INDEX], &nota->armonicos[i][REL_AMP_INDEX]))
 			return false;
 		free(linea);
 	}
@@ -43,40 +43,77 @@ bool obtener_armonicos(FILE *f, nota_t *nota){
 }
 
 
-//impresión
-void imprimir_cantidad_armonicos(nota_t * nota){
-	printf("cantidad de armónicos: %i\n", nota->cantidad_armonicos);
-}
-
-void imprimir_armonicos(nota_t * nota) {
-	for(size_t i = 0; i < nota->cantidad_armonicos; i++)
-		printf("Frec: %.1f; Amp: %.5f\n", nota->armonicos[i][REL_FREC_INDEX], nota->armonicos[i][REL_AMP_INDEX]);
-}
-
-
-
-
-static char * _leer_linea(FILE *f){
-	char *linea = malloc(sizeof(char) * 12);
-	if (linea == NULL)
-		return "\0";
-	if(fgets(linea, 20, f) == NULL){
-		free(linea);
-		return "\0";
+bool obtener_parametros_modular(FILE *f, nota_t *nota){
+	char *data = _leer_linea(f, PARAMS_LENGTH);	//malloc
+	if (data == NULL)
+		return false;
+	while(isdigit(data[0]) > 0) {
+		if((data = _leer_linea(f, PARAMS_LENGTH)) == NULL)
+			return false;
+	}									//cuando data[0] es alfabético, salgo del while y data = primer parámetro
+	for (size_t i = 0; i < 3; i++){
+		if (isalpha(data[0]) == 0 || data == NULL)
+			return false;
+		nota->parametros[i] = data;
+		if(i < 2)
+			data = _leer_linea(f, PARAMS_LENGTH);
 	}
+	return true;
+}
+
+
+
+static char * _leer_linea(FILE *f, size_t size){
+	char *linea = malloc(sizeof(char) * size);
+	fgets(linea, 20, f);
 	return linea;
 }
 
 static bool _obtener_datos_armonico(char * armonico_linea, const char * delim, float * f, float * a){
-	if (strlen(armonico_linea) != 11)
+	if (strlen(armonico_linea) != HARM_STR_LENGTH)
 		return false;
-	//size_t longitud_f = 1, longitud_a = 8;
 	char * amp_ptr = armonico_linea + 2;	//creo puntero al comienzo de la data de amplitud en la cadena
-	char * temp = malloc(sizeof(char) * 9);
+	char temp[MAX_AMP_DIGITS + 1];	// = malloc(sizeof(char) * 9);
 
 	*f = atof(strtok(armonico_linea, delim)); //guardo en f la primera cadena antes del espacio ('\b')
 	strcpy(temp, amp_ptr);	//copio a temp la cadena que queda después del espacio	
 	*a = atof(temp);
-	free(temp);
+
 	return true;  
+}
+
+/*static char * _leer_linea(FILE *f){
+	char *linea = malloc(sizeof(char) * 12);
+	if (linea == NULL)
+		return NULL;
+	if (fgets(linea, 20, f) == NULL){
+		free(linea);
+		return NULL;
+	}
+	return linea;
+}*/
+
+
+
+ /////////////////
+//impresión//////
+void imprimir_cantidad_armonicos(nota_t *nota){
+	printf("cantidad de armónicos: %i\n", nota->cantidad_armonicos);
+}
+
+void imprimir_armonicos(nota_t *nota) {
+	for(size_t i = 0; i < nota->cantidad_armonicos; i++)
+		printf("Frec: %.1f; Amp: %.5f\n", nota->armonicos[i][REL_FREC_INDEX], nota->armonicos[i][REL_AMP_INDEX]);
+}
+
+void imprimir_parametros(nota_t *nota){
+	puts("parámetros para modular:");
+	for (size_t i = 0; i < 3; i++){
+		printf("%s\n", nota->parametros[i]);
+	}
+}
+
+void destruir_parametros(nota_t *nota){
+	for (size_t i = 0; i < 3; i++)
+		free(nota->parametros[i]);
 }
