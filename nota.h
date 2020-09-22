2b22cef2629e92633baffa9ed27cd6004f8cb6f2
Luciano Jadur <66574334+lucianojadur@users.noreply.github.com>
2020-09-21 23:39:02 -0300

    Update nota.h

--- nota.h
+++ nota.h
@@ -24,9 +24,6 @@ Sus atributos guardan:
 #define MAX_NOTAS		10000
 #define FREC_INDEX		0
 #define AMP_INDEX		1
-#define MAX_FREC_DIGITS	1
-#define MAX_AMP_DIGITS	8
-#define HARM_STR_LENGTH	11
 #define PARAMS_LENGTH	25
 #define A4_FREC			440
 #define A4_OCT			4
@@ -36,37 +33,34 @@ Sus atributos guardan:
 
 typedef enum { C, C_SHARP, D, D_SHARP, E, F, F_SHARP, G, G_SHARP, A, A_SHARP, B }notas_t;
 
-typedef struct {
-	notas_t simbolo;
-	signed char octava;
-	float f;
-	float a;
-	double inicio;
-	double final;
-	bool finalizada;
-}nota_t;
+typedef struct nota nota_t;
 
 
+nota_t * nota_crear(notas_t simb, signed char oct, uint8_t i, double t0);
 
-nota_t *crear_nota(notas_t simb, signed char oct, uint8_t i, double t0);
+bool nota_aumentar_duracion(nota_t *n, float t);
 
-bool aumentar_duracion(nota_t *n, float t);
+notas_t nota_obtener_nombre(nota_t *n);
 
-bool nota_ya_existe(notas_t nombre, signed char oct, nota_t *notas[], size_t n, size_t *pos);
+signed char nota_obtener_octava(nota_t *n);
 
-size_t hallar_posicion(notas_t nombre, signed char oct, nota_t *notas[], size_t n);
+double nota_obtener_inicio(nota_t *n);
+
+double nota_obtener_final(nota_t *n);
+
+double nota_obtener_duracion(nota_t *n);
+
+double nota_obtener_amplitud(nota_t *n);
+
+double nota_obtener_frecuencia(nota_t *n);
 
 bool nota_finalizo(nota_t *nota);
 
 void nota_terminar(nota_t *n);
 
-void imprimir_nota(nota_t *nota);
+void nota_imprimir(nota_t *nota);
 
 void nota_borrar(nota_t *n);
-
-void vaciar_contenedor_notas(nota_t *notas[], size_t n);
-
-void actualizar_lista(nota_t *notas[]);
 
 
 #endif
